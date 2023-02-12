cbfe88f2484c53102786002e660a7f259fca5233
OtavioFSantos <otaviofs01@gmail.com>
2023-02-12 20:12:23 -0300

    added list styles

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import style from "./List.module.scss";
 
 function List() {
+  const tasks = [
+    {
+      task: "React",
+      time: "02:00:00",
+    },
+    {
+      task: "Typescript",
+      time: "01:00:00",
+    },
+  ];
   return (
-    <aside>
-      <h2>Studies</h2>
+    <aside className={style.listTasks}>
       <ul>
-        <li>
-          <h3>React</h3>
-          <span>02:00:00</span>
-        </li>
-        <li>
-          <h3>Typescript</h3>
-          <span>01:00:00</span>
-        </li>
+        {tasks.map((task, index) => (
+          <li key={index} className={style.item}>
+            <h3>{task.task}</h3>
+            <span>{task.time}</span>
+          </li>
+        ))}
       </ul>
     </aside>
   );
