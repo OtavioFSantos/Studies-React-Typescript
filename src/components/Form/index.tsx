c0f50dce987191b8b477b3594436541758423688
OtavioFSantos <otaviofs01@gmail.com>
2023-02-13 23:21:11 -0300

    minor changes

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,15 +3,28 @@ import Button from "../Button";
 import style from "./Form.module.scss";
 
 class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00",
+  };
+
+  addTask(ev: React.FormEvent) {
+    ev.preventDefault();
+  }
+
   render() {
     return (
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="task">Task</label>
           <input
             type="text"
             name="task"
             id="task"
+            value={this.state.task}
+            onChange={(ev) =>
+              this.setState({ ...this.state, task: ev.target.value })
+            }
             placeholder="e.g. React"
             required
           />
@@ -22,13 +35,17 @@ class Form extends React.Component {
             type="time"
             step="1"
             name="time"
+            value={this.state.time}
+            onChange={(ev) =>
+              this.setState({ ...this.state, time: ev.target.value })
+            }
             id="time"
             min="00:00:00"
             max="01:30:00"
             required
           />
         </div>
-        <Button />
+        <Button text="Register" />
       </form>
     );
   }
