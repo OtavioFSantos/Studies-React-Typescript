b89196073d9dc42ea77ff8767fa24698fb81e5df
OtavioFSantos <otaviofs01@gmail.com>
2023-02-16 17:48:07 -0300

    select task on click

--- src/components/List/Item/index.tsx
+++ src/components/List/Item/index.tsx
@@ -1,9 +1,23 @@
-import style from "../List.module.scss";
+import style from "../Item.module.scss";
 import { ITask } from "../../../types/task";
 
-export default function Item({ task, time, selected, completed, id }: ITask) {
+interface Props extends ITask {
+  selectTask: (selectedTask: ITask) => void;
+}
+
+export default function Item({
+  task,
+  time,
+  selected,
+  completed,
+  id,
+  selectTask,
+}: Props) {
   return (
-    <li className={style.item}>
+    <li
+      className={`${style.item} ${selected ? style.itemSelected : ""}`}
+      onClick={() => selectTask({ task, time, selected, completed, id })}
+    >
       <h3>{task}</h3>
       <span>{time}</span>
     </li>
