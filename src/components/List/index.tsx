2b433586f00155ceef1c68699be03a17768640ea
OtavioFSantos <otaviofs01@gmail.com>
2023-02-16 17:48:54 -0300

    select task on click

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,12 +2,17 @@ import { ITask } from "../../types/task";
 import Item from "./Item";
 import style from "./List.module.scss";
 
-function List({ tasks }: { tasks: ITask[] }) {
+interface Props {
+  tasks: ITask[];
+  selectTask: (selectedTask: ITask) => void;
+}
+
+function List({ tasks, selectTask }: Props) {
   return (
     <aside className={style.listTasks}>
       <ul>
-        {tasks.map((task, index) => (
-          <Item key={index} {...task} />
+        {tasks.map((task) => (
+          <Item selectTask={selectTask} key={task.id} {...task} />
         ))}
       </ul>
     </aside>
