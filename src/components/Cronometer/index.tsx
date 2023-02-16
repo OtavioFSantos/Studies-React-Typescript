10524d1650d346ce15019aa811afffac32201c5d
OtavioFSantos <otaviofs01@gmail.com>
2023-02-16 17:50:49 -0300

    add props and time

--- src/components/Cronometer/index.tsx
+++ src/components/Cronometer/index.tsx
@@ -1,8 +1,19 @@
+import { ITask } from "../../types/task";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Cronometer.module.scss";
+import { useState } from "react";
+import { timeToSeconds } from "../../common/utils/date";
 
-export default function Cronometer() {
+interface Props {
+  selected: ITask | undefined;
+}
+
+export default function Cronometer({ selected }: Props) {
+  const [time, setTime] = useState<number>();
+  if (selected?.time) {
+    setTime(timeToSeconds(selected.time));
+  }
   return (
     <div className={style.cronometer}>
       <p className={style.title}>Pick a card and start the cronometer</p>
