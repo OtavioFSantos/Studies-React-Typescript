f44036441c4b94a5ed6063abce61a96b0e0dfcae
OtavioFSantos <otaviofs01@gmail.com>
2023-02-15 22:55:51 -0300

    button now contains type

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import style from "./Button.module.scss";
 
-class Button extends React.Component<{ text: string }> {
+class Button extends React.Component<{
+  text: string;
+  type?: "button" | "submit" | "reset" | undefined;
+}> {
   render() {
-    return <button className={style.button}>{this.props.text}</button>;
+    const { type = "button" } = this.props;
+    return (
+      <button type={type} className={style.button}>
+        {this.props.text}
+      </button>
+    );
   }
 }
 
